8ad72376cde2cc66847126e3c89ca5599ac14478
Nefta Hernandez <neftaligow01@gmail.com>
2024-04-26 21:29:38 -0600

    Add Welcome component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Products from "./ui/Products/Products";
 import Categories from "./ui/Categories/Categories";
+import Welcome from "./ui/Home/Welcome";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-10 md:p-16">
+      <Welcome />
       <Categories />
       <Products />
     </main>
---
+++ app/ui/Home/Welcome.tsx
@@ -0,0 +1,7 @@
+export default function Welcome() {
+  return (
+    <div className="h-screen flex flex-col justify-center items-center">
+      <h1 className="text-4xl font-bold mb-28">SmileyStore, where you can be happy shopping! :&#41;</h1>
+    </div>
+  )
+}

bf6fd23b3f9403d3e8492b8f02fdb763bd2c74f8
Nefta Hernandez <neftaligow01@gmail.com>
2024-04-26 21:14:26 -0600

    introducing name

--- app/ui/Card/Card.tsx
+++ app/ui/Card/Card.tsx
@@ -9,7 +9,7 @@ interface ICard {
 
 export default function Card({ title, price, category, image }: ICard) {
   return (
-    <div className="px-2 py-2 rounded-md flex flex-col items-baseline">
+    <div className="px-2 py-2 rounded-md flex flex-col items-baseline h-full">
       <Image
         src={image[0]}
         alt={`Image for ${title}`}
@@ -17,14 +17,14 @@ export default function Card({ title, price, category, image }: ICard) {
         height={100}
         className="rounded-md"
       />
-      <div className=" flex flex-col w-full mt-2">
+      <div className="flex flex-col w-full mt-2 flex-grow">
         <p className="text-xl font-semibold">$ {price}</p>
-        <h2 className="font-medium">{title}</h2>
+        <h2 className="font-medium truncate">{title}</h2>
         <p className="text-zinc-500">{category}</p>
-        <button className="w-full bg-yellow-400 rounded-md text-zinc-950 font-semibold py-2 mt-2">
-          Add to cart
-        </button>
       </div>
+      <button className="w-full bg-yellow-400 rounded-md text-zinc-950 font-semibold py-2 mt-2">
+        Add to cart
+      </button>
     </div>
   );
 }

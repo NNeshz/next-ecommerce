ed584012578c57824d5048a528bac81eb6fbf608
Nefta Hernandez <neftaligow01@gmail.com>
2024-04-29 20:56:53 -0600

    Refactor FilterBar component and useProducts store to handle category and price filters

--- app/ui/FilterBar/FilterBar.tsx
+++ app/ui/FilterBar/FilterBar.tsx
@@ -1,35 +1,97 @@
 "use client";
 
 import { useGetCategories } from "@/hooks/useGetCategories";
+import { useProductsStore } from "@/store/useProducts";
+import { useState } from "react";
 
 export default function FilterBar() {
-  
   const categories = useGetCategories();
+  const getFilterChange = useProductsStore(
+    (state) => state.filterProductByCategory
+  );
+  const getAllProducts = useProductsStore((state) => state.getAllProducts);
+
+  const [category, setCategory] = useState<string>("");
+  const [price, setPrice] = useState<number>(0);
+
+  const handleCategoryChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setCategory(e.target.value);
+  };
+
+  const handlePriceChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setPrice(Number(e.target.value));
+  };
+
+  const handleFilterChange = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    getFilterChange(category, price);
+  };
 
   return (
-    <form className="flex flex-col w-60 pr-4">
-      <label htmlFor="" className="text-lg font-semibold">Category</label>
-      <select name="category" id="category" className="bg-black text-white mt-4">
-        <option value="all" className="text-gray-500">All</option>
+    <form className="flex flex-col w-60 pr-4" onSubmit={handleFilterChange}>
+      <label htmlFor="" className="text-lg font-semibold">
+        Category
+      </label>
+      <select
+        name="category"
+        id="category"
+        className="bg-black text-white mt-4"
+        onChange={handleCategoryChange}
+      >
+        <option value="all" className="text-gray-500">
+          All
+        </option>
         {categories.map((category) => (
-          <option key={category.id} value={category.id} className="text-gray-500">
+          <option
+            key={category.id}
+            value={category.name}
+            className="text-gray-500"
+          >
             {category.name}
           </option>
         ))}
       </select>
 
-      <label htmlFor="" className="text-lg font-semibold mt-8">Price</label>
-      <select name="price" id="price" className="bg-black text-white mt-4">
-        <option value="all" className="text-gray-500">All</option>
-        <option value="0-100" className="text-gray-500">$0 - $100</option>
-        <option value="100-200" className="text-gray-500">$100 - $200</option>
-        <option value="200-300" className="text-gray-500">$200 - $300</option>
-        <option value="300-400" className="text-gray-500">$300 - $400</option>
-        <option value="400-500" className="text-gray-500">$400 - $500</option>
-        <option value="500-i" className="text-gray-500">$501 +</option>
+      <label htmlFor="" className="text-lg font-semibold mt-8">
+        Price
+      </label>
+      <select
+        name="price"
+        id="price"
+        className="bg-black text-white mt-4"
+        onChange={handlePriceChange}
+      >
+        <option value={0} className="text-gray-500">
+          All
+        </option>
+        <option value={20} className="text-gray-500">
+          + $20
+        </option>
+        <option value={40} className="text-gray-500">
+          + $40
+        </option>
+        <option value={60} className="text-gray-500">
+          + $60
+        </option>
+        <option value={80} className="text-gray-500">
+          + $80
+        </option>
+        <option value={100} className="text-gray-500">
+          + $100
+        </option>
       </select>
-      <button type="submit" className="bg-yellow-400 text-black py-2 mt-4 font-semibold rounded-md">Search</button>
-      <button className="bg-red-500 text-black py-2 mt-4 font-semibold rounded-md">Reset</button>
+      <button
+        type="submit"
+        className="bg-yellow-400 text-black py-2 mt-4 font-semibold rounded-md"
+      >
+        Search
+      </button>
+      <button
+        className="bg-red-500 text-black py-2 mt-4 font-semibold rounded-md"
+        onClick={getAllProducts}
+      >
+        Reset
+      </button>
     </form>
   );
 }
--- store/useProducts.ts
+++ store/useProducts.ts
@@ -5,6 +5,9 @@ import { Products } from "@/app/libs/data";
 
 interface ProductsStore {
   products: IProduct[];
+}
+
+interface IFilters {
   category: string;
   price: number;
 }
@@ -12,13 +15,13 @@ interface ProductsStore {
 interface ProductsStoreActions {
   getAllProducts: () => void;
   filterProductByName: (name: string) => void;
+  filterProductByCategory: (category: string, price: number) => void;
+  filterProducts: (filterCriteria: IFilters) => void,
 }
 
 export const useProductsStore = create<ProductsStore & ProductsStoreActions>(
   (set, get) => ({
     products: [],
-    category: "",
-    price: 0,
     getAllProducts: () => {
       const allProducts = Products;
       set({ products: allProducts });
@@ -28,13 +31,28 @@ export const useProductsStore = create<ProductsStore & ProductsStoreActions>(
       const filteredProduct = products.filter((product) =>
         product.title.toLowerCase().includes(name.toLowerCase())
       );
-      set({ products: filteredProduct }); // Actualizar filteredProducts en el estado
-    },
-    filterProductByCategory: (category: string) => {
-      const filteredProduct = get().products.filter((product) => {
-        return product.category.name === category;
-      })
       set({ products: filteredProduct });
-    }
+    },
+    filterProductByCategory: (categoryName: string, price: number) => {
+      if (categoryName && price > 0) {
+        const filteredProducts = get().products.filter((product) => {
+          return (
+            product.category.name === categoryName && product.price >= price
+          );
+        });
+        set({ products: filteredProducts });
+      } else if (categoryName && !price) {
+        const filteredProducts = get().products.filter((product) => {
+          return product.category.name === categoryName;
+        });
+        set({ products: filteredProducts });
+      } else if (!categoryName && price > 0) {
+        const filteredProducts = get().products.filter((product) => {
+          return product.price >= price;
+        });
+        set({ products: filteredProducts });
+      }
+    },
+    filterProducts: (filterCriteria: IFilters) => {}
   })
 );
